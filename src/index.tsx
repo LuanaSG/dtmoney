0870580e0fd2134030bf2cbe9db081e7edce07b3
unknown <luana.souza.galvao@gmail.com>
2021-04-07 11:18:03 -0300

    Primeiro Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
+import { createServer, Model } from 'miragejs';
+
+createServer({
+  //"mirage" tem um banco de dados interno:
+
+  models: {
+    transactions: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: '04-03-2021',
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: '04-03-2021',
+        },
+      ],
+    })
+  },
+
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', schema => {
+      //console.log(schema)
+      // @ts-ignore
+      return schema.transactions.all(); // retornar todas as transações do bd
+     })
+
+     //dados enviados para "transactions" (json/javascript)
+     this.post('/transactions', (schema, request) => {
+       const data = JSON.parse(request.requestBody)
+       return schema.create('transactions', data)
+     } )
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +57,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
